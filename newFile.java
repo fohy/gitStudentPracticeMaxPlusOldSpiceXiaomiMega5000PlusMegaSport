13236d58b1e6a64f13cd7275faa23eb2ef40da72
f0hy <sashkaloel@yandex.ru>
2025-10-17 11:14:16 +0500

    commit message

---
+++ newFile.java
@@ -0,0 +1,11 @@
+public class newFile {
+    public static void main(String[] args) {
+        String name = "Студент";
+        int age = 20;
+        System.out.println("Привет, мир! Мне " + age + " лет.")
+    }
+}
+
+
+
+
