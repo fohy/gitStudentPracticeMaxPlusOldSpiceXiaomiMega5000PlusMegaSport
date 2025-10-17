a710bb8640f5affecb820e4217f33c7176b4b500
f0hy <sashkaloel@yandex.ru>
2025-10-17 11:04:29 +0500

    скебоб

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloRodl {
+    public static void main(String[] args) {
+        String name = "Студент";
+        int age = 20;
+        System.out.println("Привет, мир! Мне " + age + " лет.")
+    }
+}
